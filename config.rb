0481625a18b49270b02ad316c49bd3a4767b9987
mimpski <mimpski@hotmail.com>
2016-04-19 16:05:27 +0100

    create a basic directory of empty files and the config.rb file

---
+++ config.rb
@@ -0,0 +1,9 @@
+require 'susy'
+preferred_syntax = :scss
+http_path = '/'
+css_dir = 'assets/css'
+sass_dir = 'assets/sass'
+relative_assets = true
+line_comments = true
+sourcemap = true
+output_style = :compressed
